8a77f60bda1a34d9119ae7b2f0d719827c8b61cd
Sergey K <anaxronik@gmail.com>
2020-03-07 15:13:55 +0500

    Edit dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import css from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+
+
+const Dialog = (props) => {
+    let path = '/dialogs/' + props.id
+    return (
+        <NavLink to={path} className={css.dialog} activeClassName={css.active} >
+            {props.text}
+        </NavLink >
+    );
+}
+
+
+const Message = (props) => {
+    return (
+        <div className={css.message}>
+            {props.text}
+        </div>
+    );
+}
+
 
 const Dialogs = () => {
     return (
         <div className={css.block}>
             <div className={css.dialogs}>
-                <div className={css.dialog}>
-                    Dimich
-                </div>
-                <div className={`${css.dialog} ${css.active}`}>
-                    Andrey
-                </div>
-                <div className={css.dialog}>
-                    Sweta
-                </div>
-                <div className={css.dialog}>
-                    Sasha
-                </div>
-                <div className={css.dialog}>
-                    Victor
-                </div>
+                <h2>ДИАЛОГИ</h2>
+                <Dialog text='Димыч' id='1' />
+                <Dialog text='Andrey' id='2' />
+                <Dialog text='Sweta' id='3' />
+                <Dialog text='Sasha' id='4' />
+                <Dialog text='Victor' id='5' />
             </div>
 
             <div className={css.messages}>
-                <div className={css.message}>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas, necessitatibus beatae sed incidunt quasi et pariatur autem in rerum? Provident rem quam hic reprehenderit ab, vitae accusantium nam id.</div>
-                <div className={css.message}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur earum voluptates expedita sit. Doloribus iste sint sequi, ratione cumque laudantium commodi cupiditate velit, accusantium vel optio sit tenetur veritatis voluptas?</div>
-                <div className={css.message}>Не вызывает сомнений, что начало повседневной работы по формированию позиции играет важную роль в формировании системы обучения кадров, соответствующей насущным потребностям. Идейные соображения высшего порядка, а также постоянное информационно-пропогандистское обеспечение нашей деятельности проверки влечёт за собой интересный процесс внедрения модернизации системы обучения кадров, соответствующей насущным потребностям. Таким образом рамки и место обучения кадров создаёт предпосылки качественно новых шагов для новых принципов формирования материально-технической и кадровой базы.</div>
-                <div className={css.message}>Прежде всего понимание сущности ресурсосберегающих технологий способствует повышению качества поэтапного и последовательного развития общества. Повседневная практика показывает, что рамки и место обучения кадров способствует повышению качества модели развития. Идейные соображения высшего порядка, а также высокотехнологичная концепция общественной системы требует определения и уточнения поставленных обществом и правительством задач.</div>
-                <div className={css.message}>Идейные соображения высшего порядка, а также постоянный количественный рост и сфера нашей активности требует от нас анализа новых предложений. Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности способствует подготовке и реализации системы обучения кадров, соответствующей насущным потребностям. Таким образом управление и развитие структуры создаёт предпосылки качественно новых шагов для укрепления демократической системы.</div>
+                <Message text='asdasdasdas as dasda sdasd asd' />
+                <Message text='as45g4g 4g45g4g 4g 45g 45g 4 g45 dsd' />
+                <Message text='asd4 g45 g45 g45g4sd' />
+                <Message text='asd45 g45 g45 g45 g4 5g45 g5sd' />
             </div>
         </div>
     );
