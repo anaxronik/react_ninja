200b8075f770d9a39ea457390ec1856cf6a5109c
Sergey K <anaxronik@gmail.com>
2020-03-07 16:04:10 +0500

    edit Dialogs and Post modeules

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -23,22 +23,42 @@ const Message = (props) => {
 
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        { id: 1, name: 'Dimich' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sweta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Димыч' },
+    ]
+
+    let messageData = [
+        { id: 1, text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem provident adipisci iure inventore vitae qui quaerat, odit nam debitis ea. Veniam neque ab enim numquam explicabo at dolor sequi aperiam?' },
+        { id: 2, text: 'Следует отметить, что начало повседневной работы по формированию позиции в значительной степени обуславливает создание направлений прогрессивного развития. Повседневная практика показывает, что консультация с широким активом проверки влечёт за собой интересный процесс внедрения модернизации поэтапного и последовательного развития общества. Идейные соображения высшего порядка, а также консультация с широким активом позволяет оценить значение представляет собой интересный эксперимент новых принципов формирования материально-технической и кадровой базы.' },
+        { id: 3, text: 'Равным образом дальнейшее развитие различных форм деятельности способствует подготовке и реализации экономической целесообразности принимаемых изменений. Равным образом дальнейшее развитие различных форм деятельности требует анализа соответствующих условий активизации. Не вызывает сомнений, что социально-экономическое развитие способствует подготовке и реализации новых предложений.' },
+        { id: 4, text: 'Следует отметить, что высокотехнологичная концепция общественной системы обеспечивает широкому кругу специалистов новых принципов формирования материально-технической и кадровой базы. Равным образом высокотехнологичная концепция общественной системы играет важную роль в формировании направлений прогрессивного развития. С другой стороны высокотехнологичная концепция общественной системы способствует подготовке и реализации экономической целесообразности принимаемых изменений.' },
+        { id: 5, text: 'Следует отметить, что рамки и место обучения кадров создаёт предпосылки качественно новых шагов для форм воздействия. Следует отметить, что сложившаяся структура организации способствует повышению качества модели развития. Прежде всего консультация с широким активом способствует подготовке и реализации поэтапного и последовательного развития общества.' },
+    ]
+
     return (
         <div className={css.block}>
             <div className={css.dialogs}>
                 <h2>ДИАЛОГИ</h2>
-                <Dialog text='Димыч' id='1' />
-                <Dialog text='Andrey' id='2' />
-                <Dialog text='Sweta' id='3' />
-                <Dialog text='Sasha' id='4' />
-                <Dialog text='Victor' id='5' />
+                <Dialog text={dialogsData[0].name} id={dialogsData[0].id} />
+                <Dialog text={dialogsData[1].name} id={dialogsData[1].id} />
+                <Dialog text={dialogsData[2].name} id={dialogsData[2].id} />
+                <Dialog text={dialogsData[3].name} id={dialogsData[3].id} />
+                <Dialog text={dialogsData[4].name} id={dialogsData[4].id} />
+
             </div>
 
             <div className={css.messages}>
-                <Message text='asdasdasdas as dasda sdasd asd' />
-                <Message text='as45g4g 4g45g4g 4g 45g 45g 4 g45 dsd' />
-                <Message text='asd4 g45 g45 g45g4sd' />
-                <Message text='asd45 g45 g45 g45 g4 5g45 g5sd' />
+                <Message text={messageData[0].text} />
+                <Message text={messageData[1].text} />
+                <Message text={messageData[2].text} />
+                <Message text={messageData[3].text} />
+                <Message text={messageData[4].text} />
+
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,7 +6,7 @@ const Post = (props) => {
         <div className={css.post}>
             <img className={css.avatar} src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/280x178_2" alt="" />
             <p>{props.text}</p>
-            <span>like</span>
+            <span>like {props.likesCount}</span>
         </div>
     );
 }
