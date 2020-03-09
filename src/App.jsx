93e55818fb2e089fc3393539301a08abae6ce568
anaxronik <anaxronik@gmail.com>
2020-03-10 00:28:46 +0500

    Move up props in to index.js

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
         <Header />
         <Navbar />
-        <Route path='/profile' render={() => <Profile />} />
-        <Route path='/dialogs' render={() => <Dialogs />} />
+        <Route path='/profile' render={() => <Profile posts={props.posts} />} />
+        <Route path='/dialogs' render={() => <Dialogs dialogs={props.dialogs} messages={props.messages} />} />
         <Route path='/news' render={() => <News />} />
         <Route path='/music' render={() => <Music />} />
         <Route path='/settings' render={() => <Settings />} />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,14 +3,16 @@ import css from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
-const Profile = () => {
+const Profile = (props) => {
+
+
     return (
         <div className={css.block}>
             <div>
                 <img className={css.bigimg} src="https://pix10.agoda.net/hotelImages/5647641/-1/45ee96c8c2b177ffcd99818b6b6387f0.jpg?s=1024x768" alt="" />
             </div>
             <ProfileInfo />
-            <MyPosts />
+            <MyPosts posts={props.posts} />
         </div >
     )
 }
