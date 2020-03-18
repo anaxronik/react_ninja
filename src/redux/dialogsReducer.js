f7981fe449497b4b525a9da3e790c05c734298e9
Sergey Komarov <anaxronik@gmail.com>
2020-03-19 00:26:37 +0500

    switch state to redux

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,7 +1,43 @@
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {
+            id: 1,
+            name: 'Dimich'
+        },
+        {
+            id: 2,
+            name: 'Andrey'
+        },
+        {
+            id: 3,
+            name: 'Sweta'
+        },
+        {
+            id: 4,
+            name: 'Sasha'
+        },
+    ],
+    messages: [
+        {
+            id: 1,
+            text: 'Вакузяптра Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem provident adipisci iure inventore vitae qui quaerat, odit nam debitis ea. Veniam neque ab enim numquam explicabo at dolor sequi aperiam?'
+        },
+        {
+            id: 2,
+            text: 'Следует отметить, что начало повседневной работы по формированию позиции в значительной степени обуславливает создание направлений прогрессивного развития. Повседневная практика показывает, что консультация с широким активом проверки влечёт за собой интересный процесс внедрения модернизации поэтапного и последовательного развития общества. Идейные соображения высшего порядка, а также консультация с широким активом позволяет оценить значение представляет собой интересный эксперимент новых принципов формирования материально-технической и кадровой базы.'
+        },
+        {
+            id: 3,
+            text: 'Равным образом дальнейшее развитие различных форм деятельности способствует подготовке и реализации экономической целесообразности принимаемых изменений. Равным образом дальнейшее развитие различных форм деятельности требует анализа соответствующих условий активизации. Не вызывает сомнений, что социально-экономическое развитие способствует подготовке и реализации новых предложений.'
+        },
+    ],
+    newMessageText: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         default:
             return state
