7355cf07a40516437a7127adda4410b5bcd5be1e
Sergey Komarov <anaxronik@gmail.com>
2020-03-19 21:24:59 +0500

    Delete rerender function from index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 
-
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App />
-        </Provider>, document.getElementById('root')
-    );
-}
-
-rerenderEntireTree()
-
-store.subscribe(() => {
-    rerenderEntireTree()
-})
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
